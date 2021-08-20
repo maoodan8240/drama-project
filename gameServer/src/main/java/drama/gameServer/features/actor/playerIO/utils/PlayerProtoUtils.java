d25107e9fa927d5a5c193a178e6ecd2f919c49a6
maoodan8240 <maodan8240@sina.com>
2021-08-20 11:06:30 +0800

    update PlayerProtoUtils.java

--- gameServer/src/main/java/drama/gameServer/features/actor/playerIO/utils/PlayerProtoUtils.java
+++ gameServer/src/main/java/drama/gameServer/features/actor/playerIO/utils/PlayerProtoUtils.java
@@ -1,2 +1,17 @@
-package drama.gameServer.features.actor.playerIO.utils;public class PlayerProtoUtils {
+package drama.gameServer.features.actor.playerIO.utils;
+
+import dm.relationship.topLevelPojos.player.Player;
+import drama.protos.PlayerProtos;
+
+public class PlayerProtoUtils {
+    public static PlayerProtos.Sm_Player createSm_Player(Player player, PlayerProtos.Sm_Player.Action action) {
+        PlayerProtos.Sm_Player.Builder b = PlayerProtos.Sm_Player.newBuilder();
+        b.setAction(action);
+        b.setName(player.getBase().getName());
+        b.setSex(player.getBase().getSex());
+        b.setIcon(player.getBase().getIcon());
+        b.setBirthday(player.getBase().getBirthday());
+        b.setPlace(player.getBase().getPlace());
+        return b.build();
+    }
 }
